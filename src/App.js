a6fad49b6451ba201867299f240208c6b20f2aa6
dcnxx1 <alkisali1@hotmail.com>
2022-06-30 15:45:01 +0200

    added nav to home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './components/Home/Home'
 import Player from './components/Player/Player'
+import Nav from './components/Nav/Nav'
 function App() {
   return (
     <div className="App">
+    <Nav />
      <Home />
      <Player />
     </div>
