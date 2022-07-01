9d1beac67606644210eb1f6ccd385572aabb9c26
dcnxx1 <alkisali1@hotmail.com>
2022-07-01 17:04:40 +0200

    added hover animation on nav icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,10 @@ import { Icon, HomeWhite, PlaylistWhite, QueueWhite, LogoutWhite } from '../impo
 function Nav() {
   return (
     <nav className='Nav'>
-    <Icon className="Icon" path={HomeWhite} toolTip="Home" />
-    <Icon className="Icon" toolTip="Playlists" path={PlaylistWhite} />
-    <Icon className="Icon" toolTip="Queue" path={QueueWhite} />
-    <Icon className="Icon" toolTip="Logout" path={LogoutWhite} />
+    <Icon className="Icon tooltip" path={HomeWhite} toolTip="Home" />
+    <Icon className="Icon tooltip" toolTip="Playlists" path={PlaylistWhite} />
+    <Icon className="Icon tooltip" toolTip="Queue" path={QueueWhite} />
+    <Icon className="Icon tooltip" toolTip="Logout" path={LogoutWhite} />
     </nav>
   )
 }
--- src/compound/Icon.js
+++ src/compound/Icon.js
@@ -2,32 +2,38 @@ import styled, { keyframes } from "styled-components";
 import React from 'react';
 
 const IconTooltip = styled.div`
-background: #333;
+/* background: #333; */
 border: 2px solid white;
 width: fit-content;
 height: fit-content;
 border-radius: 15px;
-padding: 0.2rem 50px;
 display: none;
-
+user-select: none;
 `
 
 
 const IconContainer = styled.span`
 background: url(${({ pathToIcon }) => pathToIcon}) center no-repeat;
 display: block;
+width: 45px;
+height: 45px;
 cursor: pointer;
+ /* show icon tooltip on hover */
  &:hover ${IconTooltip}{
     display: block;
     position: relative;
     left: 3rem;
+ }
+ &:hover{
+    transition: border-radius 0.2s linear, transform 0.2s linear;
+    transform: scale(1.06)
  }
 
 `
 
 export default function Icon ( { children, toolTip, path, ...args } ){
     return( <IconContainer {...args} pathToIcon={path}>
-       {toolTip && (<IconTooltip>{toolTip}</IconTooltip>) }
+       {toolTip && (<IconTooltip {...args}>{toolTip}</IconTooltip>) }
     </IconContainer>)
 }
 
