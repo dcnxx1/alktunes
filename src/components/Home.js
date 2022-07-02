294aa667f36192d2ad2dc4672392687c7c440c75
dcnxx1 <alkisali1@hotmail.com>
2022-07-02 13:21:04 +0200

    added Holder inside the cover

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,8 +26,20 @@ function Home() {
       <div className='trending-holder'>
         <h2>Trending</h2>
       </div>
-      <div className="trending-track-holder">
-        
+      <div className="trending-track-holder b-test">
+        <Track>
+          <Track.Holder>
+            <Track.Cover  path="https://unsplash.it/400/400"/>
+              <Track.Holder isColumn={true}>
+                <Track.Text>Livin' life just as usual</Track.Text>
+                <Track.Text isGrey={true}>Drake</Track.Text>
+              </Track.Holder>
+          </Track.Holder>
+
+          <Track.Holder>
+            <Track.Text>Certified Loverboy</Track.Text>
+          </Track.Holder>
+        </Track>
       </div>
       
       </div>
