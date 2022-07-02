c3f09029fa97881bb854e07132a058eed58b6666
dcnxx1 <alkisali1@hotmail.com>
2022-07-02 18:35:09 +0200

    adding new styles to feature and trending

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,48 +2,14 @@ import React from 'react'
 import Search from './Search'
 import Feature from '../compound/Feature'
 import Track from '../compound/Track'
+import {MoreWhite} from '../imports'
 function Home() {
   return (
     <div className='Home '>
         <Search className="b-test" />
-      <div className='component-featured '>
-        <div className='featured-header'>
-          <h2>Featured</h2>
-        </div>
-          <div className='feature-holder '>
-            <div className='item-content'>
-              <Feature artistName="Default" imgSrc='https://unsplash.it/200/200' />
-            </div>
-            <div className='item-content '>
-              <Feature artistName="Default" imgSrc='https://unsplash.it/200/200' />
-            </div>
-            <div className='item-content '>
-              <Feature artistName="Default" imgSrc='https://unsplash.it/200/200' />
-            </div>
-          </div>
-      </div>
-      <div className='component-trending b-test'>
-      <div className='trending-holder'>
-        <h2>Trending</h2>
-      </div>
-      <div className="trending-track-holder b-test">
-        <Track>
-          <Track.Holder>
-            <Track.Cover  path="https://unsplash.it/400/400"/>
-              <Track.Holder isColumn={true}>
-                <Track.Text>Livin' life just as usual</Track.Text>
-                <Track.Text isGrey={true}>Drake</Track.Text>
-              </Track.Holder>
-          </Track.Holder>
-
-          <Track.Holder>
-            <Track.Text>Certified Loverboy</Track.Text>
-          </Track.Holder>
-        </Track>
-      </div>
+      
       
       </div>
-    </div>
   )
 }
 
--- src/compound/Track.js
+++ src/compound/Track.js
@@ -1,49 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import React from 'react';
-import Icon from "./Icon";
+import Icon from './Icon';
 import statics from "../static/statics";
-const Holder = styled.div`
-display: ${({hide}) => hide == true ? "none" : "flex"};
-flex-direction: ${({isColumn}) => isColumn == true ? "column" : "row"};
-justify-content: center;
-align-items: ${({isColumn}) => isColumn == true ? "left" : "center"};
-padding-left: ${({isColumn}) => isColumn == true ? "1rem" : "0rem"};
-flex: 1;
-border: 2px solid white;
-`
 
-const TrackContainer = styled.div`
-width: 100%;
-height: fit-content;
-display: flex;
-`
-
-const TrackCover = styled(Icon)`
-width: 75px;
-height: 75px;
-&:hover{
-    transform: none;
-    transition: none;
-}
-`
-const TrackText = styled.span`
-color: ${({isGrey}) => isGrey == true ? "#ACADAF" : "white"};
-`
-
-export default function Track ({ hide = false , children, ...args}){
-    return <TrackContainer {...args}>
-        {children}
-    </TrackContainer>
-}
-
-Track.Cover = function CoverTrack({ ...args }){
-    return <TrackCover {...args} />
-}
-
-Track.Holder = function HolderTrack ({children, hide = false, isColumn, ...args}) {
-    return <Holder {...args} isColumn={isColumn} hide={hide}>{children}</Holder>
-}
-
-Track.Text = function TextTrack({ isGrey, children, ...args}){
-    return <TrackText {...args} isGrey={isGrey} >{children}</TrackText>
-}
