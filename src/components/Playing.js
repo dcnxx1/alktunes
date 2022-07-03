d3486b36c32f96be0042de6c11354512fabb1bc2
dcnxx1 <alkisali1@hotmail.com>
2022-07-03 16:59:59 +0200

    added next on

--- src/components/Playing.js
+++ src/components/Playing.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import Track from '../compound/Track'
 function Playing() {
   return (
     <div className='Playing'>
+      <div className='nexton'>
+          <div className='playing-header'>
+          <h2>Next on playlist</h2>
+        </div>
+        <div className='playing-track'>
+          <Track/>
+          <Track/>
+          <Track/>
+          <Track/>
+          
+        </div>
+      </div>
       
     </div>
   )
--- src/compound/Track.js
+++ src/compound/Track.js
@@ -9,17 +9,17 @@ function Track() {
         <div className='track-holder '>
             <img className='track-cover-img' src='https://unsplash.it/500/500' />
         </div>
-        <div className='track-holder t-flex-col'>
+        <div className='track-holder t-flex-col track-name'>
             <span className='track-name'>SHOTTA FLOW 6</span>
             <span className='artist-name'>NLE CHOPPA</span>
         </div>
-        <div className='track-holder f-1'>
+        <div className='track-holder track-album f-1'>
             <span>Certified Loverboy</span>
         </div>
-        <div className='track-holder f-1'>
+        <div className='track-holder track-length f-1'>
             <span>2:15</span>
         </div>
-        <div className='track-holder'>
+        <div className='track-holder track-icon'>
             <Icon  path={MoreWhite}/>
         </div>
     </div>
