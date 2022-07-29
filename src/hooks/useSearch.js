94c976576c19e0cfd0790f6cf7f8cf956a49926a
dcnxx1 <alkisali1@hotmail.com>
2022-07-30 00:36:14 +0200

    adding search functionality

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,19 +1,33 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-
+import statics from '../static/statics'
 function useSearch() {
   const [search, setSearch] = useState([])
   const [searchInput, setInput] = useState('')
-
+  const [suggestResults, setSuggest] = useState([])
+  const [searchCache, setSearchCache] = useState([])
     const handleSearchInput = e => setInput(e.target.value)
+    
+    const handleIllegalInput = () => 
+    searchInput === "" || 
+    searchInput.length == 0 || 
+    statics.INPUT_VALIDATION.RESTRICT_CHARS.test(searchInput) === true ||
+    statics.INPUT_VALIDATION.STARTS_WITH_NUM.test(searchInput) === true ? false: true
+    
     function handleSubmit(e){
         e.preventDefault()
     }
 
     useEffect(() => {
         const inputSearchTimeout = setTimeout(() => {
+            if(handleIllegalInput() === false) return
+            const config = { params :{ search: searchInput } }
             
-        }, 300)
+            axios.get(`http://192.168.1.210:5055/search/`, config).then((res) => {
+                console.log(res.data)   
+            // setSearchCache()
+            })
+        }, 200)
 
         return () => {
             clearTimeout(inputSearchTimeout)
